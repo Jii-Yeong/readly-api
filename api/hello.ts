c5649a480627d794e6c647f0f9ec5ae9fca1744d
Jii-Yeong <tj5134@naver.com>
2024-10-17 18:28:00 +0900

    fix: GET 함수 따로 분리

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,3 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+export function GET(request: Request) {
+  return new Response(`Hello from Test`);
 }
